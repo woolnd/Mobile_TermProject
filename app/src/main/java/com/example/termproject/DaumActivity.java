079e503fb0f89d745e14c9328b58e3b832167ace
wodnd <wodnd0418@naver.com>
2023-12-07 12:55:09 +0900

    feat: searh website

---
+++ app/src/main/java/com/example/termproject/DaumActivity.java
@@ -0,0 +1,40 @@
+package com.example.termproject;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+public class DaumActivity extends AppCompatActivity {
+
+    private WebView webView;
+    private ImageView back;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daum);
+
+        getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.white));
+
+        back = findViewById(R.id.back);
+        webView = findViewById(R.id.google);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.loadUrl("https://www.daum.net");
+    }
+}
